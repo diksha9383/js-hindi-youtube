8c0a08608cb9b8a7e79b4f7af79da8aef1398f98
Diksha Bhardwaz <149160977+diksha9383@users.noreply.github.com>
2024-05-17 08:45:29 +0000

    datatyped

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "diksha@google.com"
+var accountPassword = "12345"
+accountCity = "Darbhanga"
+
+//accountId = 2 // not alowed
+console.log(accountId);
+accountEmail = "bhardwaz@gmail.com"
+accountPassword = "345678"
+accountCity = "Bihar"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // it will not work because we are using nodejs not browser
+
+console.log(3+3)
+console.log("Hi Diksha")
