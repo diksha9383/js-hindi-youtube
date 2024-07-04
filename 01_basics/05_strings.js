87bdfb801d3401b67e6f630e952eeae270151887
Diksha Bhardwaz <149160977+diksha9383@users.noreply.github.com>
2024-07-04 08:11:18 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,32 +1,36 @@
-const name = "diksha"
+const name = "Diksha"
 const repoCount = 50
 
-//console.log(name + repoCount + " Value");
+console.log(name + repoCount + " Value");
 console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`);
 
-const gameName = new String('diksha-db-com')
+const gameName = new String ('diksha-db')
+
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
-console.log(gameName.indexOf('s'));
+console.log(gameName.indexOf('k'));
 
-const newString = gameName.substring(0, 4)
+const newString = gameName.substring(0,4)
 console.log(newString);
 
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   diksha  "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+const newStrngOne = "   diksha  "
+console.log(newStrngOne);
+console.log(newStrngOne.trim());
 
-const url = "https://diksha.com/diksha%20bhardwaz"
-console.log(url.replace('%20', '-'))
+const url = "https://diksha.com/diksh%20bhardwaz"
+console.log(url.replace('%20', '-'));
 
-console.log(url.includes('aman'));
+console.log(url.includes('bhardwaz'));
 
 console.log(gameName.split('-'));
-console.log(gameName.small());
+
+console.log(gameName.concat());
+console.log(gameName.blink());
+
