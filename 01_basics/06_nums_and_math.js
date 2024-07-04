122033653a0411e7e815a17b111a848bdd7c49a4
Diksha Bhardwaz <149160977+diksha9383@users.noreply.github.com>
2024-07-04 08:34:07 +0000

    Math and Num

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,28 +1,25 @@
 const score = 400
 console.log(score);
-const balance = new Number (100)
+const balance = new Number(100)
 console.log(balance);
 
+console.log(balance.toString());
 console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+console.log(balance.toFixed(2));
 
-const otherNumber = 123.876
+const otherNumber = 123.8966
 console.log(otherNumber.toPrecision(3));
 
 const hundreds = 1000000
 console.log(hundreds.toLocaleString('en-IN'));
 
-////////++++++++++++++++++++++++++/////////////
+//***********Maths***************** */
+
 console.log(Math);
 console.log(Math.abs(-4));
-console.log(Math.round(5.8));
-console.log(Math.ceil(3.2));
-console.log(Math.floor(5.9));
-console.log(Math.sqrt(25));
-console.log(Math.min(5, 8, 3, 10));
-console.log(Math.max(4, 29, 2, 4));
-
-console.log(Math.random());
-
-console.log((Math.random()*10) +1);
-
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.max(4,3,6,1));
+console.log(Math.min(4,3,6,1));
+console.log((Math.random()*10) + 1);
