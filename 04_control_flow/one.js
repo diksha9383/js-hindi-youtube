f8861739323e387252963aa4ebeb98d0a252a382
Diksha Bhardwaz <149160977+diksha9383@users.noreply.github.com>
2024-07-16 13:17:20 +0000

    if_else

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,5 +1,45 @@
-const isuserloggedIn = true
+// const isuserloggedIn = true
+// const temperature = 41
 
-if(){
 
+// if(temperature===41){
+//     console.log("less than 50");
+// }
+// else{
+// console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// if(2 == "2"){
+//   console.log("executed");
+// }
+// const score = 200
+// if(score>100){
+// let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+// const balance = 1000
+// // if(balance>500) console.log("test"), console.log("test2");
+
+// if(balance<500){
+//     console.log("less than 500");
+// } else if(balance<750){
+//     console.log("less than 750");
+// } else if(balance<950){
+//     console.log("less than 950");
+// } else{
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = true
+const loggedInFromEmail = true
+if(userLoggedIn && debitCard){
+    console.log("Allow to buy course");
+}
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
 }
